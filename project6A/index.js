25b9d447eae8187e993992b97bee2f33b305408d
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-03 15:02:57 +0530

    patch,post, delete using postman

--- project6A/index.js
+++ project6A/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const fs = require('fs');
+
 
 const app = express();
 const PORT = 8000;
 
 const users = require('./MOCK_DATA.json');
+
+// MiddleWare
+app.use(express.urlencoded({ extended: false })); //This will handle the data coming from post request.
 
 // showing all users
 app.get('/api/users', (req, res) => {
@@ -21,9 +26,17 @@ app.get('/users',(req, res) => {
 })
 
 app.post('/api/users', (req, res) => {
-    // TODO: create a new user
-    return res.json({status: "Pending"});
-})
+    // req.body contains all the data from the frontend body
+    const body = req.body;
+
+    // adding json coming from POSTMAN's post request, incrementing value of id by 1.
+    users.push({...body, id: users.length + 1});
+
+    // appending the data into the file
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({status: "success", id: users.length});
+    });
+});
 
 
 //dynamic routing, ':userid' can be anything like :id, :num etc
@@ -52,12 +65,48 @@ app.route('/api/users/:userid')
     return res.json(user);
 })
 .patch((req, res) => {
-    // TODO: Update the user with id
-    return res.json({status: "Pending"});
+    const id = Number(req.params.userid);
+    const body = req.body;
+    
+    // Find the user index to update
+    const userIndex = users.findIndex((user) => user.id === id);
+    
+    if (userIndex === -1) {
+        return res.status(404).json({status: "error", message: "User not found"});
+    }
+    
+    // Update the user with new data
+    users[userIndex] = {...users[userIndex], ...body};
+    // ...users[userIndex] takes all the data of the user
+    // ...body takes all the data coming from the request & then overrite the old data with new one
+    // then assign back to the users[userIndex]
+    
+    // Write updated data back to file
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).json({status: "error", message: "Failed to update"});
+        }
+        return res.json({status: "success", user: users[userIndex]});
+    });
 })
 .delete((req, res) => {
-    // TODO: delete the user with id
-    return res.json({status: "Pending"});
+    const id = Number(req.params.userid);
+    const userIndex = users.findIndex((user) => user.id === id);
+    
+    if (userIndex === -1) {
+        return res.status(404).json({status: "error", message: "User not found"});
+    }
+    
+    // Remove the user from array
+    const deletedUser = users.splice(userIndex, 1);
+    
+    // Write updated data back to file
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
+        if (err) {
+            return res.status(500).json({status: "error", message: "Failed to delete"});
+        }
+        return res.json({status: "success", message: "User deleted", user: deletedUser[0]});
+    });
 });
 
 
